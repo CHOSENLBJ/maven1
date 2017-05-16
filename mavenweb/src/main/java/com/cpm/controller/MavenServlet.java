71a6c4fb352351ae54eec7343276acd3cfe672f5
CPM <1246087793@qq.com>
2017-05-16 11:37:37 +0800

    "test分支下的注释信息"

--- mavenweb/src/main/java/com/cpm/controller/MavenServlet.java
+++ mavenweb/src/main/java/com/cpm/controller/MavenServlet.java
@@ -12,7 +12,7 @@ public class MavenServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         String name = request.getParameter("name");
-        request.setAttribute("tip", name + " is coming...");//把提示信息存入request域中！！
+        request.setAttribute("tip", name + " is coming...");//把提示信息存入request域中！！(这是test分支下的注释信息)
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
